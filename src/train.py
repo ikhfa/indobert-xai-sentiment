dace391296691c44afc0f1984822b4ab5017d70d
Ikhsan Fanani <ikhsan.fanani@gmail.com>
2026-03-28 10:30:51 +0700

    fix tokenizer keyword

--- src/train.py
+++ src/train.py
@@ -137,7 +137,7 @@ def train() -> None:
         args=training_args,
         train_dataset=train_ds,
         eval_dataset=val_ds,
-        tokenizer=tokenizer,
+        processing_class=tokenizer,
         compute_metrics=compute_metrics,
     )
 
